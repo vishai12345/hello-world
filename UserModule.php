fd5dddc7d4cad6770eca1f7081218ec38d7c78cd
vishai12345 <vishalchaturvedi710@gmail.com>
2017-01-25 15:00:41 +0530

    update user model
    just erasing the comments

--- UserModule.php
+++ UserModule.php
@@ -1,55 +1,25 @@
 <?php
-/**
- * Yii-User module
- * 
- * @author Mikhail Mangushev <mishamx@gmail.com> 
- * @link http://yii-user.googlecode.com/
- * @license http://www.opensource.org/licenses/bsd-license.php
- * @version $Id: UserModule.php 105 2011-02-16 13:05:56Z mishamx $
- */
+
 
 class UserModule extends CWebModule
 {
-	/**
-	 * @var int
-	 * @desc items on page
-	 */
+	
 	public $user_page_size = 10;
 	
-	/**
-	 * @var int
-	 * @desc items on page
-	 */
+	
 	public $fields_page_size = 10;
 	
-	/**
-	 * @var string
-	 * @desc hash method (md5,sha1 or algo hash function http://www.php.net/manual/en/function.hash.php)
-	 */
 	public $hash='md5';
-	
-	/**
-	 * @var boolean
-	 * @desc use email for activation user account
-	 */
+
 	public $sendActivationMail=true;
 	
-	/**
-	 * @var boolean
-	 * @desc allow auth for is not active user
-	 */
+
 	public $loginNotActiv=false;
 	
-	/**
-	 * @var boolean
-	 * @desc activate user on registration (only $sendActivationMail = false)
-	 */
+	
 	public $activeAfterRegister=false;
 	
-	/**
-	 * @var boolean
-	 * @desc login after registration (need loginNotActiv or activeAfterRegister = true)
-	 */
+	
 	public $autoLogin=true;
 	
 	public $registrationUrl = array("/user/registration");
@@ -62,28 +32,16 @@ class UserModule extends CWebModule
 	
 	public $fieldsMessage = '';
 	
-	/**
-	 * @var array
-	 * @desc User model relation from other models
-	 * @see http://www.yiiframework.com/doc/guide/database.arr
-	 */
+	
 	public $relations = array();
 	
-	/**
-	 * @var array
-	 * @desc Profile model relation from other models
-	 */
+	
 	public $profileRelations = array();
 	
-	/**
-	 * @var boolean
-	 */
+	
 	public $captcha = array('registration'=>true);
 	
-	/**
-	 * @var boolean
-	 */
-	//public $cacheEnable = false;
+	
 	
 	public $tableUsers = '{{users}}';
 	public $tableProfiles = '{{profiles}}';
@@ -93,18 +51,12 @@ class UserModule extends CWebModule
 	static private $_admin;
 	static private $_admins;
 	
-	/**
-	 * @var array
-	 * @desc Behaviors for models
-	 */
+
 	public $componentBehaviors=array();
 	
 	public function init()
 	{
-		// this method is called when the module is being created
-		// you may place code here to customize the module or the application
-
-		// import the module-level models and components
+		
 		$this->setImport(array(
 			'user.models.*',
 			'user.components.*',
@@ -131,19 +83,12 @@ class UserModule extends CWebModule
 			return false;
 	}
 	
-	/**
-	 * @param $str
-	 * @param $params
-	 * @param $dic
-	 * @return string
-	 */
+	
 	public static function t($str='',$params=array(),$dic='user') {
 		return Yii::t("UserModule.".$dic, $str, $params);
 	}
 	
-	/**
-	 * @return hash string.
-	 */
+	
 	public static function encrypting($string="") {
 		$hash = Yii::app()->getModule('user')->hash;
 		if ($hash=="md5")
@@ -154,10 +99,7 @@ class UserModule extends CWebModule
 			return hash($hash,$string);
 	}
 	
-	/**
-	 * @param $place
-	 * @return boolean 
-	 */
+	
 	public static function doCaptcha($place = '') {
 		if(!extension_loaded('gd'))
 			return false;
@@ -166,10 +108,6 @@ class UserModule extends CWebModule
 		return false;
 	}
 	
-	/**
-	 * Return admin status.
-	 * @return boolean
-	 */
 	public static function isAdmin() {
 		if(Yii::app()->user->isGuest)
 			return false;
@@ -184,10 +122,7 @@ class UserModule extends CWebModule
 		}
 	}
 
-	/**
-	 * Return admins.
-	 * @return array syperusers names
-	 */	
+	
 	public static function getAdmins() {
 		if (!self::$_admins) {
 			$admins = User::model()->active()->superuser()->findAll();
@@ -199,9 +134,7 @@ class UserModule extends CWebModule
 		return self::$_admins;
 	}
 	
-	/**
-	 * Send mail method
-	 */
+	
 	public static function sendMail($email,$subject,$message) {
     	$adminEmail = Yii::app()->params['adminEmail'];
 	    $headers = "MIME-Version: 1.0\r\nFrom: $adminEmail\r\nReply-To: $adminEmail\r\nContent-Type: text/html; charset=utf-8";
@@ -210,11 +143,7 @@ class UserModule extends CWebModule
 	    return mail($email,'=?UTF-8?B?'.base64_encode($subject).'?=',$message,$headers);
 	}
 	
-	/**
-	 * Return safe user data.
-	 * @param user id not required
-	 * @return user object or false
-	 */
+	
 	public static function user($id=0) {
 		if ($id) 
 			return User::model()->active()->findbyPk($id);
@@ -229,11 +158,7 @@ class UserModule extends CWebModule
 		}
 	}
 	
-	/**
-	 * Return safe user data.
-	 * @param user id not required
-	 * @return user object or false
-	 */
+	
 	public function users() {
 		return User;
 	}
